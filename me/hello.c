d909b848d65893299f4d1a71cb6b2643f06f54d4
Filipe Franca <dev.filipefranca@gmail.com>
2025-08-14 13:00:01 +0000

    Thu, Aug 14, 2025, 10:00 AM -03:00

--- me/hello.c
+++ me/hello.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 // inicia o programa
 int main(void)
 {
-    string nome = get_string("digite seu nome: "); // variável de texto string para digitar o seu nome
+    string nome =
+        get_string("digite seu nome: "); // variável de texto string para digitar o seu nome
 
-    printf("hello, %s\n", nome);// imprime no terminal seu nome usando o %s, com o \n para mudar de linha
+    printf("hello, %s\n",
+           nome); // imprime no terminal seu nome usando o %s, com o \n para mudar de linha
 }
