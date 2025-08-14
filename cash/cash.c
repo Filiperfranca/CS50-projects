403deca8717756b00970ecc7b29df6b7687e355d
Filipe Franca <dev.filipefranca@gmail.com>
2025-08-14 14:16:50 +0000

    Thu, Aug 14, 2025, 11:16 AM -03:00

---
+++ cash/cash.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    #define quarters 25
+    #define dimes 10
+    #define nickels 5
+    #define pennies 1
+    
+    int troco;
+    do
+    {
+        troco = get_int("digite o valor do troco: ");
+    }
+    while (troco < 0);
+
+    int moedas usadas = 0
+
+
+
+}
