6549724af7785f7bf69188f0c65fdf188b569dc5
Filipe Franca <dev.filipefranca@gmail.com>
2025-08-14 13:33:24 +0000

    Thu, Aug 14, 2025, 10:33 AM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,25 +6,23 @@ int main(void)
     int altura;
     do
     {
-        altura = get_int("digite a altura aqui: ");
+        altura = get_int("digite a altura aqui: "); // entrada da altura da piramide
     }
-    while(altura < 1);
+    while (altura < 1); // se o valor da piramide for menos que 1. ele volta para o "do" e pede novamente
 
-    for (int j = 1; j <= altura; j++)
+    for (int j = 1; j <= altura; j++) // laço principal que define cada linha
     {
 
-        for (int k = 0; k < altura - j; k++)
+        for (int k = 0; k < altura - j; k++) // laço dos espaços, ele pega o valor de j subtrai da altura e imprime o valor de espaços que sempre vai 1 menor que j
         {
             printf(" ");
         }
 
-        for (int i = 0; i < j; i++)
+        for (int i = 0; i < j; i++) //ele imprime o valor de j só que em "#"
         {
             printf("#");
         }
 
         printf("\n");
-        
     }
-
 }
