5fc4b77b921bb643d2bc1dc1035ba9ede43d8851
Filipe Franca <dev.filipefranca@gmail.com>
2025-09-30 11:35:44 +0000

    Tue, Sep 30, 2025, 8:35 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -12,7 +12,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             BYTE vermelho_original = image[i][j].rgbtRed;
             BYTE verde_original = image[i][j].rgbtGreen;
 
-            int conversão = round(((float)azul_original + vermelho_original + verde_original) / 3.0);
+            int conversão =
+                round(((float) azul_original + vermelho_original + verde_original) / 3.0);
 
             image[i][j].rgbtBlue = conversão;
             image[i][j].rgbtRed = conversão;
@@ -33,19 +34,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             BYTE vermelho_original = image[i][j].rgbtRed;
             BYTE verde_original = image[i][j].rgbtGreen;
 
-            int sepia_vermelho = round(.393 * vermelho_original + .769 * verde_original + .189 * azul_original);
-            int sepia_verde = round(.349 * vermelho_original + .686 * verde_original + .168 * azul_original);
-            int sepia_azul = round(.272 * vermelho_original + .534 * verde_original + .131 * azul_original);
+            int sepia_vermelho =
+                round(.393 * vermelho_original + .769 * verde_original + .189 * azul_original);
+            int sepia_verde =
+                round(.349 * vermelho_original + .686 * verde_original + .168 * azul_original);
+            int sepia_azul =
+                round(.272 * vermelho_original + .534 * verde_original + .131 * azul_original);
 
-            if(sepia_vermelho > 255)
+            if (sepia_vermelho > 255)
             {
                 sepia_vermelho = 255;
             }
-            if(sepia_verde > 255)
+            if (sepia_verde > 255)
             {
                 sepia_verde = 255;
             }
-            if(sepia_azul > 255)
+            if (sepia_azul > 255)
             {
                 sepia_azul = 255;
             }
@@ -76,5 +80,42 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp_image[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float vermelho_temp = 0;
+            float verde_temp = 0;
+            float azul_temp = 0;
+            int pixels_contados = 0;
+
+            for (int horizontal = i - 1; horizontal <= i + 1; horizontal++)
+            {
+                for (int vertical = j - 1; vertical <= j + 1; vertical++)
+                {
+                    if (horizontal >= 0 && horizontal < height && vertical >= 0 && vertical < width)
+                    {
+                        vermelho_temp += image[horizontal][vertical].rgbtRed;
+                        verde_temp += image[horizontal][vertical].rgbtGreen;
+                        azul_temp += image[horizontal][vertical].rgbtBlue;
+                        pixels_contados++;
+                    }
+                }
+            }
+            temp_image[i][j].rgbtRed = round(vermelho_temp / pixels_contados);
+            temp_image[i][j].rgbtGreen = round(verde_temp / pixels_contados);
+            temp_image[i][j].rgbtBlue = round(azul_temp / pixels_contados);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp_image[i][j];
+        }
+    }
     return;
 }
