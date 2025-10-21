e51aca446b74c7d3792fa9f85897f426156e45b5
Filipe Franca <dev.filipefranca@gmail.com>
2025-10-21 12:34:11 +0000

    Tue, Oct 21, 2025, 9:34 AM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -13,3 +13,17 @@ for char in texto:
 
     if char == '.' or char == '!' or char == '?':
         frases += 1
+
+L = letras / palavras * 100
+S = frases / palavras * 100
+
+resultado = 0.0588 * L - 0.296 * S - 15.8
+
+if resultado >= 16:
+    print("Grade 16+")
+
+elif resultado < 1:
+    print("Before Grade 1")
+
+else:
+    print(f"grade {int(round(resultado))}")
