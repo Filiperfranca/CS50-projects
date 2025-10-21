cee2461080c0e2cc900ae698738a6df0af5d0636
Filipe Franca <dev.filipefranca@gmail.com>
2025-10-21 11:43:59 +0000

    Tue, Oct 21, 2025, 8:43 AM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,14 +3,18 @@ dimes = 10
 nickels = 5
 pennies = 1
 
+moedas_usadas = 0
+
 while True:
     try:
         troco = float(input("digite o valor do troco: "))
 
         if troco > 0:
             break
+        else:
+            print("digite um valor acima de 0")
 
     except ValueError:
         print("digite um valor acima de 0")
 
-print(troco)
+
