658e8506fb7ad25389e9c3f96611e6ed4139efea
Filipe Franca <dev.filipefranca@gmail.com>
2025-12-08 13:32:49 +0000

    Mon, Dec 8, 2025, 10:32 AM -03:00

--- finance/app.py
+++ finance/app.py
@@ -35,7 +35,8 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    
+    linhas = db.execute("SELECT symbol, SUM(shares) AS total_shares FROM transactions WHERE user_id = ? GROUP BY symbol", session["user_id"])
+
     return apology("TODO")
 
 
