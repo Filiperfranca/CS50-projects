851c2d3221c405b615b9cf02cd69ee988462d78b
Filipe Franca <dev.filipefranca@gmail.com>
2025-12-10 13:11:01 +0000

    Wed, Dec 10, 2025, 10:11 AM -03:00

--- project/app.py
+++ project/app.py
@@ -6,11 +6,12 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
+# Lookup and USD removed, I won't use them.
 
 # Configure application
 app = Flask(__name__)
 
-# 
+# app.jinja_env.filters["usd"] = usd; removed
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
